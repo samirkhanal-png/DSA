26281457e057d55b867ce1597ffb87c30cd4b7be
Samir Khanal <samirkhanaal9@gmail.com>
2025-01-10 20:49:08 +0530

    Update Celebrity_Problem.cpp

--- StackNQueue/Celebrity_Problem.cpp
+++ StackNQueue/Celebrity_Problem.cpp
@@ -56,6 +56,90 @@ why?
  2 1 0 0 knows 1 can't be a celebrity
  3 1 0 0
 
+ Since one element is known by every other element except itself 
+ every other element except itself can't satisfy the condition that it must not know anyone 
+
+ 
+
+*/
+
+class Solution {
+  public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& mat) {
+        int size=mat.size();
+        int top=0;
+        int down=mat.size()-1;
+        while(top<down){
+            if(mat[top][down]==1){
+                top++;
+            }
+            else if(mat[down][top]==1){
+                down--;
+            }
+            else{ //if both are zero
+                top++;
+                down--;
+            }
+        }
+        
+        if(top>down)return -1;
+        
+        //if top==down
+        //this may or maynot be the celebrity
+        for(int i=0;i<size;i++){
+           if(i==top)continue;
+           if(mat[top][i]==0&&mat[i][top]==1){}
+           else return -1;
+        }
+        return top;
+    }
+};
+
+/*
+Brute force solution
+
+Traversing over every row and items in every row
+
+row=>I know other 
+
+col=>others know Me
+
+If I do not know anyone (that means in every row there is 0)
+
+and n-1 people except me know me (that means in every items in the column there must be 1)
+
+There can be min zero celebrity and at max one celebrity
+
+why?
+ 0 1 2 3 
+ 0 0 0 0   top
+ 1 1 0 0 knows 1 can't be a celebrity
+ 2 1 0 0 knows 1 can't be a celebrity
+ 3 1 0 0   down
+ 
+ In optimal solution we use the idea of elimination
+ 
+ making top and down looks at every possible combination of it if it accepted and not eliminated
+ 
+ One among mat[top][down] or mat[down][top] must be 1 and other must be 0 to be a celebrity
+ 
+ The one having 0 and 1 may be a celebrity 
+ 
+ It signifies may be it does not know anyone but is known by other 
+ 
+ If one of them is 1 it means he is not a celebrity for sure 
+ 
+ so eliminating
+ 
+ If both are 0 then maybe it does not know anyone but it is not known by element except itself
+ 
+ at last top crosses down or top and down becomes same
+ 
+ if crosses then return -1 there is no celebrity
+ 
+ else there is no solution
+
 */
 
 
